c9df7ecbc635e932402689b2648e35005eaf8e7c
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-09-09 13:16:51 +0200

    add footer-mid

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -29,10 +29,65 @@ export default {};
       </div>
     </section>
     <section class="footer-mid">
-      <div class="contacts"></div>
+      <div class="contacts">
+        <div class="links">
+          <div class="column">
+            <ul>
+              <li class="title">DC COMICS</li>
+              <li><a href="">Character</a></li>
+              <li><a href="">Comics</a></li>
+              <li><a href="">Movies</a></li>
+              <li><a href="">Tv</a></li>
+              <li><a href="">Games</a></li>
+              <li><a href="">Videos</a></li>
+              <li><a href="">News</a></li>
+            </ul>
+            <ul>
+              <li class="title">SHOP</li>
+              <li><a href="">Shop DC</a></li>
+              <li><a href="">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+          <ul>
+            <li class="title">DC</li>
+            <li><a href="">Terms Of Use</a></li>
+            <li><a href="">Privacy policy (New)</a></li>
+            <li><a href="">Ad Choices</a></li>
+            <li><a href="">Advertising</a></li>
+            <li><a href="">Jobs</a></li>
+            <li><a href="">Subscriptions</a></li>
+            <li><a href="">Talent Workshops</a></li>
+            <li><a href="">CPSC Certeficates</a></li>
+            <li><a href="">Ratings</a></li>
+            <li><a href="">Shop Help</a></li>
+            <li><a href="">Contact Us</a></li>
+          </ul>
+          <ul>
+            <li class="title">SITES</li>
+            <li><a href="">DC</a></li>
+            <li><a href="">MAD Magazine</a></li>
+            <li><a href="">DC Kids</a></li>
+            <li><a href="">DC Universe</a></li>
+            <li><a href="">DC Power Visa</a></li>
+          </ul>
+        </div>
+        <div class="image">
+          <img src="../assets/img/dc-logo-bg.png" alt="" />
+        </div>
+      </div>
     </section>
     <section class="footer-bot">
-      <div class="socials"></div>
+      <div class="socials">
+        <button>SIGN-UP NOW!</button>
+        <div class="social">
+          <h3>FOLLOW US</h3>
+          <img src="../assets/img/footer-facebook.png" alt="" />
+          <img src="../assets/img/footer-twitter.png" alt="" />
+          <img src="../assets/img/footer-youtube.png" alt="" />
+          <img src="../assets/img/footer-pinterest.png" alt="" />
+          <img src="../assets/img/footer-periscope.png" alt="" />
+        </div>
+      </div>
     </section>
   </footer>
 </template>
@@ -62,14 +117,65 @@ export default {};
       white-space: nowrap;
       font-size: 0.7rem;
     }
-    .footer-mid {
-      background-image: url("../assets/img/footer-bg.jpg");
-      height: 16rem;
+  }
+}
+.footer-mid {
+  background-image: url("../assets/img/footer-bg.jpg");
+  height: 16rem;
+  overflow: hidden;
+  .contacts {
+    height: 16rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    margin: auto;
+  }
+  .links {
+    display: flex;
+    gap: 1.5rem;
+    .column {
+      display: flex;
+      flex-direction: column;
     }
-    .footer-bot {
-      height: 5rem;
-      background-color: gray;
-      margin-bottom: 2rem;
+    .title {
+      font-weight: 700;
+      color: white;
+      margin-bottom: 0.5rem;
+      margin-top: 0.5rem;
+    }
+    li {
+      list-style: none;
+      a {
+        text-decoration: none;
+        color: gray;
+        font-size: 0.7rem;
+      }
+    }
+  }
+}
+.footer-bot {
+  background-color: rgb(48, 48, 48);
+  .socials {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1200px;
+    margin: auto;
+    height: 5rem;
+    button {
+      background-color: transparent;
+      color: white;
+      border: 1px solid blue;
+      padding: 0.7rem;
+    }
+    .social {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+    }
+    h3 {
+      color: blue;
     }
   }
 }
