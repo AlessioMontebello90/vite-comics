628a1b1981f487b0156b45ac151b3477f34b3040
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-09-09 12:28:26 +0200

    fix structure and  add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 import AppFooter from "./components/AppFooter.vue";
-import AppHeader from "./components/AppHeader.vue";
+
 export default {
   components: { AppHeader, AppFooter, AppMain },
 };
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,48 +1,47 @@
+<script>
+export default {};
 </script>
 
 <template>
-
-    <header>
-        <img src="../assets/img/dc-logo.png" alt="">
-        <ul class="head_list">
-            <li>CHARACTER</li>
-            <li>COMICS</li>
-            <li>MOVIES</li>
-            <li>TV</li>
-            <li>GAMES</li>
-            <li>COLLECTIBLES</li>
-            <li>VIDEOS</li>
-            <li>FANS</li>
-            <li>NEWS</li>
-            <li>SHOP</li>
-        </ul>
-    </header>
+  <header>
+    <img src="../assets/img/dc-logo.png" alt="" />
+    <ul class="head_list">
+      <li>CHARACTER</li>
+      <li>COMICS</li>
+      <li>MOVIES</li>
+      <li>TV</li>
+      <li>GAMES</li>
+      <li>COLLECTIBLES</li>
+      <li>VIDEOS</li>
+      <li>FANS</li>
+      <li>NEWS</li>
+      <li>SHOP</li>
+    </ul>
+  </header>
 </template>
 
 <style lang="scss" scoped>
-@use './../assets/styles/partials/variables.scss' as *;
+@use "./../assets/styles/partials/variables.scss" as *;
 header {
-    background-color: white;
-    height: 6rem;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-top: 2rem;
-    
+  background-color: white;
+  height: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 2rem;
 }
 .head_list {
-    list-style: none;
-    display: flex;
-    justify-content: flex-end;
-    padding-inline: 1rem;
-    font-size: .7rem;
+  list-style: none;
+  display: flex;
+  justify-content: flex-end;
+  padding-inline: 1rem;
+  font-size: 0.7rem;
 }
 li {
-    padding-inline: 1rem;
+  padding-inline: 1rem;
 }
 img {
-    width: 6%;
-    padding-left: 1rem;
+  width: 6%;
+  padding-left: 1rem;
 }
-
 </style>
