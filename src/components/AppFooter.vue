c2a8e4e9de966f8da963db240ecb1912751522f7
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-09-09 12:54:20 +0200

    add footer-up

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,26 +4,73 @@ export default {};
 
 <template>
   <footer>
-    <div class="options"></div>
-    <div class="contacts"></div>
-    <div class="socials"></div>
+    <section class="footer-up">
+      <div class="options">
+        <div class="option">
+          <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+          <p>DIGITAL COMICS</p>
+        </div>
+        <div class="option">
+          <img src="../assets/img/buy-comics-merchandise.png" alt="" />
+          <p>MERCHANDISE</p>
+        </div>
+        <div class="option">
+          <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+          <p>SUBSCRIPTION</p>
+        </div>
+        <div class="option">
+          <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
+          <p>COMIC SHOP LOCATOR</p>
+        </div>
+        <div class="option">
+          <img src="../assets/img/buy-dc-power-visa.svg" alt="" />
+          <p>DC POWER VISA</p>
+        </div>
+      </div>
+    </section>
+    <section class="footer-mid">
+      <div class="contacts"></div>
+    </section>
+    <section class="footer-bot">
+      <div class="socials"></div>
+    </section>
   </footer>
 </template>
 
 <style lang="scss" scoped>
 @use "./../assets/styles/partials/variables.scss" as *;
-.options {
+.footer-up {
   background-color: blue;
-  height: 8rem;
-  display: flex;
-}
-.contacts {
-  background-image: url("../assets/img/footer-bg.jpg");
-  height: 16rem;
-}
-.socials {
-  height: 5rem;
-  background-color: gray;
-  margin-bottom: 2rem;
+  .options {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 8rem;
+    max-width: 1200px;
+    margin: auto;
+    .option {
+      display: flex;
+      align-items: center;
+      padding-inline: 1.5rem;
+    }
+    img {
+      width: 2.2rem;
+    }
+    p {
+      color: white;
+      padding-inline: 0.7rem;
+      white-space: nowrap;
+      font-size: 0.7rem;
+    }
+    .footer-mid {
+      background-image: url("../assets/img/footer-bg.jpg");
+      height: 16rem;
+    }
+    .footer-bot {
+      height: 5rem;
+      background-color: gray;
+      margin-bottom: 2rem;
+    }
+  }
 }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,19 +4,21 @@ export default {};
 
 <template>
   <header>
-    <img src="../assets/img/dc-logo.png" alt="" />
-    <ul class="head_list">
-      <li>CHARACTER</li>
-      <li>COMICS</li>
-      <li>MOVIES</li>
-      <li>TV</li>
-      <li>GAMES</li>
-      <li>COLLECTIBLES</li>
-      <li>VIDEOS</li>
-      <li>FANS</li>
-      <li>NEWS</li>
-      <li>SHOP</li>
-    </ul>
+    <nav>
+      <img src="../assets/img/dc-logo.png" alt="" />
+      <ul class="head_list">
+        <li>CHARACTER</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -24,6 +26,10 @@ export default {};
 @use "./../assets/styles/partials/variables.scss" as *;
 header {
   background-color: white;
+}
+nav {
+  max-width: 1200px;
+  margin: auto;
   height: 6rem;
   display: flex;
   align-items: center;
