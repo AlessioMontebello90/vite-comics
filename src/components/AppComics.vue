e5493dbdbc41d7d38f977cbaf4669f9dcf822416
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-09-11 15:40:04 +0200

    add Appcomics and v-for

---
+++ src/components/AppComics.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  props: {
+    urlImg: String,
+    series: String,
+    price: String,
+  },
+};
+</script>
+
+<template>
+  <div class="cards">
+    <img :src="urlImg" :alt="series" />
+    <div class="text-container">
+      <h3>{{ series }}</h3>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,29 +1,108 @@
 <script>
-export default {};
+import AppComics from "./AppComics.vue";
+export default {
+  components: { AppComics },
+  data() {
+    return {
+      arrComics: [
+        {
+          thumb: "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+          price: "$19.99",
+          series: "Action Comics",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+          price: "$3.99",
+          series: "American Vampire 1976",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+          price: "$16.99",
+          series: "Aquaman",
+          type: "graphic novel",
+        },
+        {
+          thumb: "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+          price: "$2.99",
+          series: "Batgirl",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+          price: "$3.99",
+          series: "Batman",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+          price: "$2.99",
+          series: "Batman Beyond",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+          price: "$3.99",
+          series: "Batman/Superman",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+          price: "$4.99",
+          series: "Batman/Superman Annual",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+          price: "$5.99",
+          series: "Batman: The Joker War Zone",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+          price: "$6.99",
+          series: "Batman: Three Jokers",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+          price: "$4.99",
+          series: "Batman: White Knight Presents: Harley Quinn",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+          price: "$16.99",
+          series: "Catwoman",
+          type: "graphic novel",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
   <main>
     <div class="contents">
-      <h1>il contenuto andrà qui</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
-        minima dolor aut quia amet ab ipsum rem modi hic repellendus recusandae
-        nostrum quas eum! Adipisci ullam culpa repellat vel laborum. Architecto
-        rem, illum labore minima, porro magnam ipsum pariatur eveniet similique
-        error voluptatem quae excepturi velit maxime! Adipisci, inventore earum
-        similique unde est rem vero animi quam ullam, blanditiis dolores!
-        Corrupti consectetur accusamus in sequi nemo ab laboriosam explicabo.
-        Autem, ut sint, dolore nam quod cum deleniti vitae, in et itaque velit
-        voluptatibus consectetur modi temporibus laudantium eligendi pariatur
-        molestias. Quas, sapiente obcaecati minus, nostrum corrupti optio magni,
-        quibusdam rerum eos eum ratione voluptatibus? Aperiam ad eligendi, esse
-        accusamus atque libero facilis ab praesentium illum perferendis
-        veritatis blanditiis officia quos. Odit incidunt recusandae aspernatur
-        debitis officiis labore soluta doloribus minus nesciunt tempora error
-        aperiam dolorem exercitationem excepturi ullam placeat numquam, fuga
-        nulla magni tempore quia pariatur! Hic unde commodi mollitia!
-      </p>
+      <app-comics
+        v-for="comic in arrComics"
+        :key="comic.series"
+        :urlImg="comic.thumb"
+        :price="comic.price"
+        :series="comic.series"
+      />
     </div>
   </main>
 </template>
@@ -34,20 +113,9 @@ main {
   background-color: rgb(28, 28, 28);
   .contents {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex-shrink: 1;
-    color: white;
+
     max-width: 1200px;
     margin: auto;
-    h1 {
-      color: red;
-      &:hover {
-        cursor: pointer;
-        color: white;
-      }
-    }
   }
 }
 </style>
